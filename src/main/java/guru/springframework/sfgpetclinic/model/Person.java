298677c1033f215929c6538bf56ddea392a45e1f
Gurkan Sokat <gsokat@thrivetech.com>
2021-08-09 23:43:28 -0700

    Adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String laastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLaastName() {
+        return laastName;
+    }
+
+    public void setLaastName(String laastName) {
+        this.laastName = laastName;
+    }
+}
